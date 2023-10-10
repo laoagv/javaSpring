8d6b030063c5891db8ebba8283f6a723b36c819f
laoagv <laoagvik@gmail.com>
2023-10-10 18:00:53 +0500

    Added skillbox mod1
    Added skillbox mod1

---
+++ Skillbox/mod1/mod1_lesson6/src/main/java/org/example/app/services/BookRepository.java
@@ -0,0 +1,49 @@
+package org.example.app.services;
+
+import org.apache.log4j.Logger;
+import org.example.web.dto.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BookRepository implements ProjectRepository<Book> {
+
+    private final Logger logger = Logger.getLogger(BookRepository.class);
+    private final List<Book> repo = new ArrayList<>();
+
+    @Override
+    public List<Book> retreiveAll() {
+        return new ArrayList<>(repo);
+    }
+
+    @Override
+    public void store(Book book) {
+        book.setId(book.hashCode());
+        logger.info("store new book: " + book);
+        repo.add(book);
+    }
+
+    @Override
+    public boolean removeItemById(Integer bookIdToRemove) {
+        for (Book book : retreiveAll()) {
+            if (book.getId().equals(bookIdToRemove)) {
+                logger.info("remove book completed: " + book);
+                return repo.remove(book);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItemRegex(String bookRegexToRemove) {
+        for (Book book:retreiveAll()){
+            if ((book.getAuthor().equals(bookRegexToRemove)||book.getSize().toString().equals(bookRegexToRemove)||book.getTitle().equals(bookRegexToRemove))){
+                logger.info("remove book completed: " + book);
+                return repo.remove(book);
+            }
+        }
+        return false;
+    }
+}
